95f5fe64d2072b13b59ec6018e0c1384358c6bd3
Rayan <rayan.sayah@student.howest.be>
2026-02-15 08:25:35 +0100

    The necessary changes have been added to make it so that you can run this on a local server

--- Minigin/TextObject.cpp
+++ Minigin/TextObject.cpp
@@ -4,7 +4,7 @@
 #include "Renderer.h"
 #include "Font.h"
 #include "Texture2D.h"
-#include "FPSCOmponent.h"
+#include "FPSComponent.h"
 
 dae::TextObject::TextObject(const std::string& text, std::shared_ptr<Font> font, const SDL_Color& color)
 	: m_needsUpdate(true), m_text(text), m_color(color), m_font(std::move(font)), m_textTexture(nullptr)
