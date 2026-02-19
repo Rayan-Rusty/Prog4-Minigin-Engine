5e7e20114f069d75acbb10ba50543538f6f5e7d2
Rayan <rayan.sayah@student.howest.be>
2026-02-19 19:30:38 +0100

    FPSComponent is now a component of a gameobject that requires a TextComponent to show itself

--- Minigin/FPSComponent.cpp
+++ Minigin/FPSComponent.cpp
@@ -5,19 +5,25 @@
 #include "FPSComponent.h"
 #include "TextComponent.h"
 #include <format>
-dae::FPSComponent::FPSComponent(TextComponent* textObject)
-    : m_textObject(textObject)
+dae::FPSComponent::FPSComponent(GameObject* pOwner)
+    : Component(pOwner)
 {
-
+    m_textObject = GetOwner()->GetComponent<TextComponent>();
+    if (!m_textObject) GetOwner()->AddComponent(std::unique_ptr<TextComponent>());
 }
 
 void dae::FPSComponent::Update(float deltaTime)
 {
 
-    if (!m_textObject || deltaTime <= 0.0f) return;
+    if (!m_textObject || deltaTime <= .0f) return;
 
     float fps { 1.f / deltaTime};
     m_textObject->SetText(std::format("FPS: {:.1f}" , fps ));
     //TODO checkout wether its correctly doing the fps
 
+}
+
+std::type_index dae::FPSComponent::GetType() const
+{
+    return typeid(FPSComponent);
 }
--- Minigin/FPSComponent.h
+++ Minigin/FPSComponent.h
@@ -4,21 +4,25 @@
 
 #pragma once
 #include <string>
+
+#include "Component.h"
 #include "FPSComponent.h"
 
 namespace dae
 {
     class TextComponent;
-
-    class FPSComponent
+    class GameObject;
+    class FPSComponent final : public Component
     {
     public:
-        explicit FPSComponent(TextComponent* textObject);
+        explicit FPSComponent(GameObject* pOwner);
         FPSComponent(const FPSComponent&) = delete;
         ~FPSComponent() = default;
         FPSComponent& operator=(const FPSComponent&) = delete;
 
         virtual void Update(float deltaTime);
+
+        std::type_index GetType() const override;
     private:
         TextComponent* m_textObject{nullptr};
     };
