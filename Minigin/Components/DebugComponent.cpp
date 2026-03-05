e6af6274ffcaf6d5cafa452ddb8a64e2faaf81eb
Rayan <rayan.sayah@student.howest.be>
2026-03-05 21:15:45 +0100

    ILgui logic needs to go in DebugComponent

---
+++ Minigin/Components/DebugComponent.cpp
@@ -0,0 +1,5 @@
+//
+// Created by omen on 05/03/2026.
+//
+
+#include "DebugComponent.h"
---
+++ Minigin/Components/DebugComponent.h
@@ -0,0 +1,17 @@
+//
+// Created by omen on 05/03/2026.
+//
+
+#ifndef MINIGIN_DEBUGCOMPONENT_H
+#define MINIGIN_DEBUGCOMPONENT_H
+
+
+class DebugComponent
+{
+public:
+private:
+
+};
+
+
+#endif //MINIGIN_DEBUGCOMPONENT_H
