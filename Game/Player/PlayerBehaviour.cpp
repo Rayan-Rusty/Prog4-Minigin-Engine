597c4ba2f9e87ff79115f3dc135882afbf25ea0f
Rayan <rayan.sayah@student.howest.be>
2026-05-14 02:22:52 +0200

    Added PlayerBehvaiour

---
+++ Game/Player/PlayerBehaviour.cpp
@@ -0,0 +1,38 @@
+//
+// Created by omen on 02/05/2026.
+//
+
+#include "PlayerBehaviour.h"
+
+#include "States/PlayerIdleState.h"
+
+
+DigDug::PlayerBehaviour::PlayerBehaviour(dae::GameObject* owner)
+    :Component(owner)
+ {
+     ChangeState(std::make_unique<PlayerIdleState>());
+ }
+
+
+
+void  DigDug::PlayerBehaviour::Update(float dt)
+ {
+     if (m_state)
+         m_state->Update(*this , dt);
+ }
+
+
+void  DigDug::PlayerBehaviour::ChangeState(std::unique_ptr<State<PlayerBehaviour>> newState)
+ {
+     if (m_state)
+         m_state->Exit(*this);
+
+     m_state = std::move(newState);
+     m_state->Enter(*this);
+ }
+
+
+std::type_index  DigDug::PlayerBehaviour::GetType() const
+ {
+     return typeid(PlayerBehaviour);
+ }
