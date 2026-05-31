387db0041beae7fedbded7f96fa6445ffbe3cc0e
Rayan <rayan.sayah@student.howest.be>
2026-06-01 01:36:19 +0200

    HealthComp added

---
+++ Minigin/Components/HealthComponent.cpp
@@ -0,0 +1,5 @@
+//
+// Created by omen on 31/05/2026.
+//
+
+#include "HealthComponent.h"
---
+++ Minigin/Components/HealthComponent.h
@@ -0,0 +1,13 @@
+//
+// Created by omen on 31/05/2026.
+//
+
+#ifndef MINIGIN_HEALTHCOMPONENT_H
+#define MINIGIN_HEALTHCOMPONENT_H
+
+
+class HealthComponent {
+};
+
+
+#endif //MINIGIN_HEALTHCOMPONENT_H
