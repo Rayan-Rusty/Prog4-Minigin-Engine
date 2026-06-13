db197a5cccf195d09a433eb53fb57d148ccfabe1
Rayan <rayan.sayah@student.howest.be>
2026-06-13 06:58:41 +0200

    added health comp with lifes

--- Minigin/Components/Health/HealthComponent.cpp
+++ Minigin/Components/Health/HealthComponent.cpp
@@ -3,3 +3,67 @@
 //
 
 #include "HealthComponent.h"
+
+dae::HealthComponent::HealthComponent(GameObject *pOwner, int maxhealth, int maxLives)
+    : Component(pOwner) , m_maxHealth(maxhealth) , m_curHealth(maxhealth),  m_maxLives(maxLives), m_remainingLives(maxLives)
+{
+}
+
+int dae::HealthComponent::GetCurrentHealth()
+{
+    return m_curHealth;
+}
+
+int dae::HealthComponent::GetMaxHealth()
+{
+    return m_maxHealth;
+}
+
+void dae::HealthComponent::DoDamage(int damage)
+{
+    int oldhealth{m_curHealth};
+    m_curHealth = damage;
+    if (m_curHealth <= oldhealth)
+    {
+        m_curHealth = 0;
+
+        if (oldhealth > 0)
+        {
+            LoseLife();
+        }
+    }
+}
+
+void dae::HealthComponent::ResetResetHealth()
+{
+}
+
+void dae::HealthComponent::LoseLife()
+{
+    if (m_remainingLives > 0)
+    {
+        --m_remainingLives;
+        m_curHealth = m_maxHealth;
+    }
+}
+
+void dae::HealthComponent::GainLife(int amnt)
+{
+    if (m_remainingLives >= m_maxLives) return;
+
+    if (m_remainingLives == 0)
+    {
+        m_curHealth = m_maxHealth;
+    }
+
+    m_remainingLives += amnt ;
+    if (m_remainingLives > m_maxLives)
+    {
+        m_remainingLives = m_maxLives;
+    }
+}
+
+std::type_index dae::HealthComponent::GetType() const
+{
+    return std::type_index(typeid(HealthComponent));
+}
--- Minigin/Components/Health/HealthComponent.h
+++ Minigin/Components/Health/HealthComponent.h
@@ -4,17 +4,34 @@
 
 #ifndef MINIGIN_HEALTHCOMPONENT_H
 #define MINIGIN_HEALTHCOMPONENT_H
+#include "Component.h"
 
 namespace dae
 {
-    class HealthComponent
+    class HealthComponent : public Component
     {
     public:
+        HealthComponent(GameObject* pOwner, int maxhealth , int maxLives);
+
+        ~HealthComponent() = default;
+
         int GetCurrentHealth();
-        void ChangeHealth(int health);
+        int GetMaxHealth();
         void DoDamage(int damage);
         void ResetResetHealth();
+
+        void LoseLife();
+        void GainLife(int amount);
+
+        std::type_index GetType() const override;
+
     private:
+
+        const int m_maxHealth;
+        int m_curHealth;
+
+        const int m_maxLives;
+        int m_remainingLives;
     };
 }
 
